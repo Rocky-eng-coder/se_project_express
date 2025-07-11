2dc4ebcdedba9d27372658ca8ff0ffa9890b35e8
Rockyroad <Racquelbryanteng@email.com>
2025-07-10 21:04:34 -0400

    changes to fullstack project

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,6 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
 
-const UnauthorizedError = require("../utils/errors/unauthorized-error");
-
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 

a75375c0b933df8d18e39810b992cf2d06996687
Rockyroad <Racquelbryanteng@email.com>
2025-10-27 19:37:31 -0400

    changes to WTWR application

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -8,28 +8,20 @@ const {
   likeItem,
   dislikeItem,
 } = require("../controllers/clothingItems");
-// CRUD
 
 const {
   validateClothingItem,
   validateIdParam,
 } = require("../middlewares/validation");
-// Create
 
 router.post("/", auth, validateClothingItem, createItem);
 
-// Read
-
 router.get("/", getItems);
-
-// Delete
 
 router.delete("/:itemId", auth, validateIdParam, deleteItem);
 
-// Like
 router.put("/:itemId/likes", auth, validateIdParam, likeItem);
 
-// Dislike
 router.delete("/:itemId/likes", auth, validateIdParam, dislikeItem);
 
 module.exports = router;
